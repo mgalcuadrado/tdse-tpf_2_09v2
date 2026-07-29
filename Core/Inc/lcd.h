b4ea2a0a94c1e86f36b6b97f9461942d7127b891
DazzlingLion <118118223+DazzlingLion@users.noreply.github.com>
2026-07-29 03:54:37 -0300

    Me olvide el .h

---
+++ Core/Inc/lcd.h
@@ -0,0 +1,22 @@
+#ifndef INC_LCD_H_
+#define INC_LCD_H_
+
+#include "stm32f1xx_hal.h"
+
+// Dirección física I2C del adaptador PCF8574 (0x27 desplazado 1 bit a la izquierda)
+// Si la pantalla no enciende/muestra texto, probar cambiando a (0x3F << 1)
+#define LCD_I2C_ADDR (0x27 << 1)
+
+
+void lcdInicializar(I2C_HandleTypeDef *hi2c);
+void lcdBorrar(void);
+void lcdSetearCursor(uint8_t col, uint8_t fil);
+
+
+void lcdPrint(char *cadena);
+
+//Funciones de bajo nivel
+void lcdMandarComando(char cmd);
+void lcdMandarDato(char data);
+
+#endif /* INC_LCD_H_ */
