01162b0baf95367e541b0da8a9d86cc24beccbe5
clogabay <118193944+clogabay@users.noreply.github.com>
2026-08-09 17:50:00 -0300

    Add files via upload

--- pote.c
+++ pote.c
@@ -1,68 +1,56 @@
 #include "pote.h"
 #include <stdlib.h>
+
 extern uint16_t adc_buffer[3];
 
-/*
- * Conversión ADC 12 bits
- * a posición matriz 32x32
- */
-
-uint8_t adc_a_posicion(uint16_t valor_raw){
+/*Conversión ADC 12 bits a posición matriz 32x32*/
+uint8_t adc_a_posicion(uint16_t valor_raw) {
     return (uint8_t)((valor_raw * 32) / 4096);
 }
 
-/*
- * Conversión ADC 12 bits
- * a brillo 0-255
- */
-
-uint8_t adc_a_brillo(uint16_t valor_raw){
+/*Conversión ADC 12 bits a brillo 0-255*/
+uint8_t adc_a_brillo(uint16_t valor_raw) {
     return (uint8_t)(valor_raw >> 4);
 }
 
-/*
- * Filtro con histéresis
- */
-
-uint8_t aplicar_filtro_ruido(uint8_t nuevo,
-                             uint8_t anterior){
-
-    if(abs((int)nuevo - (int)anterior) <= 2){
+/*Filtro con histéresis*/
+uint8_t aplicar_filtro_ruido(uint8_t nuevo, uint8_t anterior) {
+    if (abs((int)nuevo - (int)anterior) <= 2) {
         return anterior;
     }
     return nuevo;
 }
 
-void leer_potenciometros(Potenciometros_t *pots){
-    uint8_t x_raw;
-    uint8_t y_raw;
-    uint8_t brillo_raw;
-    /*
-     * Leer DMA
-     */
+void leer_potenciometros(uint8_t *r, uint8_t *g, uint8_t *b) {
+    uint8_t r_raw;
+    uint8_t g_raw;
+    uint8_t b_raw;
 
-    x_raw =
-    adc_a_posicion(adc_buffer[0]);
-    y_raw =
-    adc_a_posicion(adc_buffer[1]);
-    brillo_raw =
-    adc_a_brillo(adc_buffer[2]);
-    /*
-     * Aplicar filtro
-     */
+    static uint8_t r_anterior = 0;
+    static uint8_t g_anterior = 0;
+    static uint8_t b_anterior = 0;
 
-    pots->x = aplicar_filtro_ruido(x_raw, pots->x);
-    pots->y =
-    aplicar_filtro_ruido(y_raw, pots->y);
-    pots->brillo = aplicar_filtro_ruido(brillo_raw, pots->brillo);
+    /*Leer DMA*/
+    r_raw = adc_a_posicion(adc_buffer[0]);
+    g_raw = adc_a_posicion(adc_buffer[1]);
+    b_raw = adc_a_brillo(adc_buffer[2]);
+
+    /* Aplicar filtro*/
+    r_anterior = aplicar_filtro_ruido(r_raw, r_anterior);
+    g_anterior = aplicar_filtro_ruido(g_raw, g_anterior);
+    b_anterior = aplicar_filtro_ruido(b_raw, b_anterior);
+
+    /*Devolver los valores mediante punteros*/
+    *r = r_anterior;
+    *g = g_anterior;
+    *b = b_anterior;
 }
 
-bool posicion_cambio(uint8_t x, uint8_t y){
-
+bool posicion_cambio(uint8_t x, uint8_t y) {
     static uint8_t old_x = 255;
     static uint8_t old_y = 255;
 
-    if(x != old_x || y != old_y){
+    if (x != old_x || y != old_y) {
         old_x = x;
         old_y = y;
         return true;
--- pote.h
+++ pote.h
@@ -5,27 +5,10 @@
 #include <stdbool.h>
 
 
-typedef struct
-{
-    uint16_t x;
-    uint16_t y;
-    uint16_t brillo;
-
-} Potenciometros_t;
-
-
-
-void leer_potenciometros(Potenciometros_t *pots);
-
-
+void leer_potenciometros(uint16_t *r, uint16_t *g, uint16_t *b);
 uint8_t adc_a_posicion(uint16_t valor_raw);
-
-
 uint8_t adc_a_brillo(uint16_t valor_raw);
-
-
-bool posicion_cambio(uint8_t x,
-                     uint8_t y);
+bool posicion_cambio(uint8_t x, uint8_t y);
 
 
 #endif
