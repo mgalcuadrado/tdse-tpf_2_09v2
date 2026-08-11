92d1f854c9947198e67d3b91f50450a7075b0533
Ace <111801820+juanbautista-ace@users.noreply.github.com>
2026-08-11 18:22:36 -0300

    Update potenciometro.h

--- Core/Inc/potenciometro.h
+++ Core/Inc/potenciometro.h
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 
 
-void leer_potenciometros(uint16_t *r, uint16_t *g, uint16_t *b);
+void leer_potenciometros(uint8_t *r, uint8_t *g, uint8_t *b);
 uint8_t adc_a_posicion(uint16_t valor_raw);
 uint8_t adc_a_brillo(uint16_t valor_raw);
 bool posicion_cambio(uint8_t x, uint8_t y);
