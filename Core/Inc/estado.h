b78ae832592566bfc72a55ac57cf126be06ef7c5
DazzlingLion <118118223+DazzlingLion@users.noreply.github.com>
2026-08-11 14:02:20 -0300

    Reemplazo estado.h

--- Core/Inc/estado.h
+++ Core/Inc/estado.h
@@ -8,14 +8,17 @@
 #define ESTADO_H
 
 #include "boton.h"
+#include "matriz.h"
 
 typedef enum {
     ESTADO_MENU_PRINCIPAL = 0,
     ESTADO_MENU_DIBUJO,
+	ESTADO_LIMPIAR_DIBUJO,
     ESTADO_DIBUJANDO,
     ESTADO_CAMBIANDO_PINCEL,
     ESTADO_MENU_SECUENCIA,
-    ESTADO_COMPLETANDO_SECUENCIA
+    ESTADO_COMPLETANDO_SECUENCIA,
+	ESTADO_LIMPIAR_SECUENCIA
 } EstadoSistema_t;
 
 // Llamar una sola vez al arrancar, después de los inits de HAL/periféricos
@@ -26,5 +29,7 @@ void sistemaTick(BotonEvento_t input);
 
 // Usada por los módulos de menú para pedir una transición de pantalla
 void sistemaCambiarEstado(EstadoSistema_t nuevo_estado);
+
+Matriz_t* sistemaObtenerMatrizActiva(void);
 
 #endif
