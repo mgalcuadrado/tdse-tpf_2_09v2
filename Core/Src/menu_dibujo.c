5b5cfe561889c5df40537062322c98e612b3c5b1
Ace <111801820+juanbautista-ace@users.noreply.github.com>
2026-08-17 19:07:37 -0300

    Add files via upload

--- Core/Src/menu_dibujo.c
+++ Core/Src/menu_dibujo.c
@@ -43,7 +43,7 @@ void menuDibujoEntrar(void) {
 }
 
 void menuDibujoMostrar(char seleccion[6], int indice_seleccion) {
-	lcdVaciarBuffer();
+	//lcdVaciarBuffer();
     char mensaje0[LCD_COLUMNAS];
     char mensaje1[LCD_COLUMNAS];
     char mensaje2[LCD_COLUMNAS];
@@ -54,18 +54,19 @@ void menuDibujoMostrar(char seleccion[6], int indice_seleccion) {
         snprintf(mensaje2, sizeof(mensaje2), "(%c) Guardar Dibujo", seleccion[2]);
 
         lcdBufferearLinea(0, mensaje0);
-
         lcdBufferearLinea(1, mensaje1);
-
         lcdBufferearLinea(2, mensaje2);
+        lcdBufferearLinea(3, " ");
+
 
     } else if (2 < indice_seleccion && indice_seleccion < 5) {
         snprintf(mensaje0, sizeof(mensaje0), "(%c) Cargar Dibujo", seleccion[3]);
         snprintf(mensaje1, sizeof(mensaje1), "(%c) Cambiar Pincel", seleccion[4]);
 
         lcdBufferearLinea(0, mensaje0);
-        lcdBufferearLinea(1, mensaje1);
-		
+		lcdBufferearLinea(1, mensaje1);
+		lcdBufferearLinea(2, " ");
+		lcdBufferearLinea(3, " ");
     } else {
         printf("Error en el Display/MenuDibujo \n");
     }
