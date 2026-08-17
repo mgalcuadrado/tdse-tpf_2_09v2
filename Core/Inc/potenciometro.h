a8d63ef21c1c44708d5c0622e6f825c732020f41
clogabay <118193944+clogabay@users.noreply.github.com>
2026-08-17 15:00:31 -0300

    Update potenciometro.h

--- Core/Inc/potenciometro.h
+++ Core/Inc/potenciometro.h
@@ -1,29 +1,25 @@
-#ifndef potenciometro_h
+#ifndef potenciometro_H
 #define potenciometro_h
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "main.h"
 
-typedef struct {
+
+typedef struct
+{
     uint8_t r;
     uint8_t g;
     uint8_t b;
+
 } Potenciometros_t;
 
+
 void pote_init(void);
+
+uint8_t adc_a_color(uint16_t valor_raw);
 
 void escrutar_potenciometros(void);
 
 Potenciometros_t procesar_potenciometros(void);
-
-/* Convierte un valor ADC a una posición de la matriz */
-uint8_t adc_a_posicion(uint16_t valor_raw);
-
-/* Convierte un valor ADC a brillo */
-uint8_t adc_a_brillo(uint16_t valor_raw);
-
-/* Determina si hubo un cambio de posición */
-bool posicion_cambio(uint8_t x, uint8_t y);
 
 #endif
